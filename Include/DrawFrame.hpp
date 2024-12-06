a71e23d88404a695c18721912be66b4e9f2626aa
erostedt <rostedteric@gmail.com>
2024-12-06 10:06:34 +0100

    move out implementation to cpp file

--- Include/DrawFrame.hpp
+++ Include/DrawFrame.hpp
@@ -1,38 +1,15 @@
 #pragma once
 
-#include "Window.hpp"
-
 namespace xmaze
 {
+class Window;
+
 class DrawFrame
 {
   public:
-    DrawFrame(xmaze::Window &window) : m_Window(window)
-    {
-        auto display = m_Window.GetDisplay();
-        auto gc = m_Window.GetGC();
-        auto screen = m_Window.GetScreen();
-        auto rect = m_Window.GetActiveArea();
-        XSetForeground(display, gc, WhitePixel(display, screen));
-        XFillRectangle(display, m_Window.GetPixmap(), gc, rect.Left(), rect.Top(), rect.Width(), rect.Height());
-        XSetForeground(display, gc, BlackPixel(display, screen));
-    }
-
-    ~DrawFrame()
-    {
-        auto display = m_Window.GetDisplay();
-        auto gc = m_Window.GetGC();
-        auto rect = m_Window.GetActiveArea();
-        auto pixmap = m_Window.GetPixmap();
-
-        XFlush(display);
-        XCopyArea(display, pixmap, m_Window.GetNativeWindow(), gc, 0, 0, rect.Width(), rect.Height(), 0, 0);
-    }
-
-    const xmaze::Window &GetWindow() const
-    {
-        return m_Window;
-    }
+    DrawFrame(xmaze::Window &window);
+    ~DrawFrame();
+    const xmaze::Window &GetWindow() const;
 
   private:
     xmaze::Window &m_Window;
---
+++ Src/DrawFrame.cpp
@@ -0,0 +1,34 @@
+#include "DrawFrame.hpp"
+#include "Window.hpp"
+
+namespace xmaze
+{
+
+DrawFrame::DrawFrame(xmaze::Window &window) : m_Window(window)
+{
+    auto display = m_Window.GetDisplay();
+    auto gc = m_Window.GetGC();
+    auto screen = m_Window.GetScreen();
+    auto rect = m_Window.GetActiveArea();
+    XSetForeground(display, gc, WhitePixel(display, screen));
+    XFillRectangle(display, m_Window.GetPixmap(), gc, rect.Left(), rect.Top(), rect.Width(), rect.Height());
+    XSetForeground(display, gc, BlackPixel(display, screen));
+}
+
+DrawFrame::~DrawFrame()
+{
+    auto display = m_Window.GetDisplay();
+    auto gc = m_Window.GetGC();
+    auto rect = m_Window.GetActiveArea();
+    auto pixmap = m_Window.GetPixmap();
+
+    XFlush(display);
+    XCopyArea(display, pixmap, m_Window.GetNativeWindow(), gc, 0, 0, rect.Width(), rect.Height(), 0, 0);
+}
+
+const xmaze::Window &DrawFrame::GetWindow() const
+{
+    return m_Window;
+}
+
+}; // namespace xmaze
